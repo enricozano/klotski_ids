2e35a58d15fb6bbcefce4dbe65403d12869747b4
SansonEris <sansoneris5@gmail.com>
2023-05-02 21:33:00 +0200

    Struttura progetto

---
+++ klotski_ids/src/main/java/com/example/klotski_ids/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.klotski_ids;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ klotski_ids/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.klotski_ids {
+    requires javafx.controls;
+    requires javafx.fxml;
+        requires javafx.web;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+            requires net.synedra.validatorfx;
+            requires org.kordamp.ikonli.javafx;
+            requires org.kordamp.bootstrapfx.core;
+            requires eu.hansolo.tilesfx;
+            requires com.almasb.fxgl.all;
+    
+    opens com.example.klotski_ids to javafx.fxml;
+    exports com.example.klotski_ids;
+}
